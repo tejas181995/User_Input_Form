185311ffec6b9208d8b868fdff663bf7fa3669d5
Tejas <tejasdev95@gmail.com>
2021-04-16 20:07:43 +0530

    Added method to check validatation of email

--- form.js
+++ form.js
@@ -1,11 +1,22 @@
 const text = document.querySelector('#text');
 const text_error = document.querySelector('.text-error');
+const email = document.querySelector('#email');
+const email_error = document.querySelector('.email-error');
+
 text.addEventListener('input', function() {
     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,14}$");
     if (nameRegex.test(text.value))
         text_error.textContent = "";
     else
         text_error.textContent = "name is not valid";
+});
+
+email.addEventListener('input', function() {
+    let emailRegex = RegExp("^[A-Za-z0-9+-]+(\\.[A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]{2,}){1,2}$");
+    if (emailRegex.test(email.value))
+        email_error.textContent = "";
+    else
+        email_error.textContent = "Invalid Email";
 });
 
 const salary = document.querySelector('#salary');
