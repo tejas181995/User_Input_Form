f5ff1815e8a33d851dcf167e5807a823623933b8
Tejas <tejasdev95@gmail.com>
2021-04-16 20:22:25 +0530

    Added password validation method

--- form.js
+++ form.js
@@ -4,6 +4,8 @@ const email = document.querySelector('#email');
 const email_error = document.querySelector('.email-error');
 const telephone = document.querySelector('#tel');
 const telephone_error = document.querySelector('.tel-error');
+const pwd = document.querySelector('#pwd');
+const pwd_error = document.querySelector('.pwd-error');
 
 text.addEventListener('input', function() {
     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,14}$");
@@ -26,10 +28,16 @@ telephone.addEventListener('input', function() {
     if (telephoneRegex.test(telephone.value))
         telephone_error.textContent = "";
     else
-        telephone_error.textContent = "Invalid Email";
+        telephone_error.textContent = "Invalid telephone number";
 });
 
-
+pwd.addEventListener('input', function() {
+    let pwdRegex = RegExp("^[A-za-z0-9]{8,}$");
+    if (pwdRegex.test(pwd.value))
+        pwd_error.textContent = "";
+    else
+        pwd_error.textContent = "Invalid password";
+});
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
